d7017134bfec3240bff71e67d9a9a81b8d6ed136
sfsheng0322 <sfsheng0322@gmail.com>
2015-11-05 21:14:12 +0800

    MVP框架完善

---
+++ app/src/main/java/com/sun/bingo/control/SingleControl.java
@@ -0,0 +1,15 @@
+package com.sun.bingo.control;
+
+import com.sun.bingo.framework.base.BaseControl;
+import com.sun.bingo.framework.proxy.MessageProxy;
+
+/**
+ * Created by sunfusheng on 15/11/5.
+ */
+public class SingleControl extends BaseControl {
+
+    public SingleControl(MessageProxy mMessageCallBack) {
+        super(mMessageCallBack);
+    }
+
+}
