2248f298d849677d6b5afb12a4436003d8d411d8
sfsheng0322 <sfsheng0322@gmail.com>
2016-03-07 10:18:31 +0800

    GitHub第三方登录

--- app/src/main/java/com/sun/bingo/constant/ConstantParams.java
+++ app/src/main/java/com/sun/bingo/constant/ConstantParams.java
@@ -2,20 +2,11 @@ package com.sun.bingo.constant;
 
 public interface ConstantParams {
 
-    /**
-     * Bmob正式环境
-     */
+    // Bmob信息
     String BMOB_APP_ID = "52676e9314981e9d2f7a068e71a8e85a";
     String SMS_LOGIN_VERIFY_CODE = "Bingo登录";
 
-    /**
-     * https://fir.im
-     *
-     * id 应用ID，可在"应用管理"->"基本信息"查看
-     * short 应用短链接，上传应用时会随机生成，用户可修改
-     * api_token 用于识别用户身份, api_token 可以在[用户信息]中生成和刷新
-     * release_id Release 表示一个版本的唯一 ID
-     */
+    // fir.im信息
     String FIR_IM_ID = "55d40a70748aac58a6000047";
     String FIR_IM_API_TOKEN = "071e514d156c894cc3d73ed2d9c8b538";
 
@@ -26,5 +17,9 @@ public interface ConstantParams {
     String SINA_SCOPE = "email,direct_messages_read,direct_messages_write,"
             + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
             + "follow_app_official_microblog," + "invitation_write";
+
+    // GitHub信息
+    String GITHUB_CLIENT_ID = "c2a4749964d945858564";
+    String GITHUB_CLIENT_SECRET = "337baf3350475f409be888de1ddb35f59f2c7b7a";
 
 }
