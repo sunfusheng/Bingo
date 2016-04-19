a4fcb94950b2ff41f571055646bd458f95a38c60
sfsheng0322 <sfsheng0322@gmail.com>
2016-04-19 12:49:57 +0800

    更新Download Apk Dialog

--- DroidFramework/src/main/java/com/framework/dialog/DownloadDialog.java
+++ DroidFramework/src/main/java/com/framework/dialog/DownloadDialog.java
@@ -1,6 +1,7 @@
 package com.framework.dialog;
 
 import android.content.Context;
+import android.content.DialogInterface;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
@@ -25,6 +26,12 @@ public class DownloadDialog {
             materialDialog = new MaterialDialog.Builder(mContext)
                     .content("正在下载...")
                     .progress(false, 100, false)
+                    .cancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            ToastTip.show("已进入后台下载");
+                        }
+                    })
                     .build();
         }
         if (!materialDialog.isShowing()) {
